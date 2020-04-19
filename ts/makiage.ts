1f237206662d705e7e8462f8aae46a6cd9d3d249
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-04-19 17:32:59 +0000

    make makiage test

---
+++ ts/makiage.ts
@@ -0,0 +1,11 @@
+function oldFunction() {
+  console.log(`巻き上げテスト ${v}`);
+  var v = "小松菜";
+}
+oldFunction();
+
+function letFunction() {
+  console.log(`巻き上げテスト ${v}`);
+  let v = "小松菜";
+}
+letFunction();
