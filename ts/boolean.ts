ef05562920be8c45c60515442c1836f96b3965a6
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-04-20 00:07:13 +0000

    make boolean sample

---
+++ ts/boolean.ts
@@ -0,0 +1,18 @@
+//値を表示
+console.log(true);
+console.log(false);
+
+//変数にbooleanを代入
+const flag: boolean = true;
+
+//他データに変換
+console.log(flag.toString());
+console.log(String(flag));
+console.log(Number(flag));
+
+//他のデータ型をtrue/falseに変換
+// const notEnpty - Boolean("test string");
+// const flagment = flagStr === 'true';
+// const str = "not empty string";
+// const isEmpty = !str;
+// const notEnpty2 - !!str;
