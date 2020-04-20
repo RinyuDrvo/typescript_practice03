be205d7e0e3da87216764a25331b091f7ff841bf
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-04-20 00:28:14 +0000

    make number sample

---
+++ ts/number.ts
@@ -0,0 +1,11 @@
+const year: number = 2019;
+
+//他データ形に変換
+console.log(year.toString());
+console.log(year.toString(2));
+console.log(Boolean(year));
+
+//他データ型を数値に変換
+console.log(parseInt("010"));
+console.log(parseInt("010", 8));
+console.log(Number(true));
